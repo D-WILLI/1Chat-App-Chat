969b6dbbe906ae62e74c800d2d0e5d01af8d8c22
Diamond <williams.diamond2392@gmail.com>
2021-08-19 08:53:29 -0400

    commit

---
+++ chat-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Chat from '.components/chat'
+
+function App() {
+  return (
+    <div class="container">
+            <h1 id="home"> Diamond Williams </h1>
+            <h4> Welcome to my Portfolio  </h4>
+            <Chat />
+ 
+            
+            
+    </div>
+  );
+}
+
+
+export default App;
+
